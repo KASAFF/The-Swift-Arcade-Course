74f54b62629b95ebccfc5876e4cdb39924c8dbee
KASAFF <91468100+KASAFF@users.noreply.github.com>
2023-01-03 14:38:59 +0300

    Made a change

--- Bankey/BankeyUnitTests/AccountSummary/AccountSummaryViewControllerTests.swift
+++ Bankey/BankeyUnitTests/AccountSummary/AccountSummaryViewControllerTests.swift
@@ -7,17 +7,17 @@
 
 import Foundation
 import XCTest
-
+// change1
 @testable import Bankey
 
 class AccountSummaryViewControllerTests: XCTestCase {
     var vc: AccountSummaryViewController!
     var mockManager: MockProfileManager!
-    
+
     class MockProfileManager: ProfileManageable {
         var profile: Profile?
         var error: NetworkError?
-        
+
         func fetchProfile(forUserId userId: String, completion: @escaping (Result<Profile, NetworkError>) -> Void) {
             if error != nil {
                 completion(.failure(error!))
@@ -27,40 +27,40 @@ class AccountSummaryViewControllerTests: XCTestCase {
             completion(.success(profile!))
         }
     }
-    
+
     override func setUp() {
         super.setUp()
         vc = AccountSummaryViewController()
         // vc.loadViewIfNeeded()
-        
+
         mockManager = MockProfileManager()
         vc.profileManager = mockManager
     }
-    
+
     func testTitleAndMessageForServerError() throws {
         let titleAndMessage = vc.titleAndMessageForTesting(for: .serverError)
         XCTAssertEqual("Server Error", titleAndMessage.0)
         XCTAssertEqual("Ensure you are connected to the internet. Please try again.", titleAndMessage.1)
     }
-    
+
     func testTitleAndMessageForDecodingError() throws {
         let titleAndMessage = vc.titleAndMessageForTesting(for: .decodingError)
         XCTAssertEqual("Decoding Error", titleAndMessage.0)
         XCTAssertEqual("We could not process your request. Please try again.", titleAndMessage.1)
     }
-    
+
     func testAlertForServerError() throws {
         mockManager.error = NetworkError.serverError
         vc.forceFetchProfile()
-        
+
         XCTAssertEqual("Server Error", vc.errorAlert.title)
         XCTAssertEqual("Ensure you are connected to the internet. Please try again.", vc.errorAlert.message)
     }
-    
+
     func testAlertForDecodingError() throws {
         mockManager.error = NetworkError.decodingError
         vc.forceFetchProfile()
-        
+
         XCTAssertEqual("Decoding Error", vc.errorAlert.title)
         XCTAssertEqual("We could not process your request. Please try again.", vc.errorAlert.message)
     }
